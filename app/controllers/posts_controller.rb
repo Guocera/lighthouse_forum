caed312cbe4e36b7b9baeec8a93233ee2262017d
Nick Yee <njsyee@gmail.com>
2016-01-30 08:48:59 -0700

    Added update feature.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -15,7 +15,20 @@ class PostsController < ApplicationController
     else
       render :new
     end
+  end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+
+    if @post.update_attributes(post_params)
+      redirect_to posts_path
+    else
+      render :edit
+    end
   end
 
   def show
